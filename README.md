5fc107955dd7ec7d9092d86757487f0d65bd25b4
Maxim Deb Natkh <debnatkh@gmail.com>
2021-02-22 15:10:46 +0300

    added splitting big input files into several chunks and processing them independently

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # trim-silence
+This script can be used to trim silence from video.
+
+It detects silent (configured by `SILENCE_THRESH` param) parts, whose duration exceeds `MIN_SILENCE_LEN` ms. 
+If there are k nonsilent parts, i.e. [a1:b1], [a2:b2], ..., [ak:bk], then the resulting video is a concatenation of 
+parts [a1-m:b1+m], [a2-m:b2+m], ..., [ak-m,bk+m], where m = `MARGIN`.
+
+In order to bypass some limitations `N_SEGMENTS` = 10 is used, which means that the input video will be split into 
+`N_SEGMENTS` chunks firstly, then each chunk processed independently and in the end all results will be concatenated.
+
 ## Usage:
 ```
-usage: main.py [-h] -i INFILE [-o OUTFILE] [-s MIN_SILENCE_LEN] [-t SILENCE_THRESH] [-m MARGIN] [-d]
+usage: main.py [-h] -i INFILE [-o OUTFILE] [-s MIN_SILENCE_LEN] [-t SILENCE_THRESH] [-m MARGIN]
+               [-n N_SEGMENTS] [-d]
 
 Trim silence from video.
 
@@ -14,9 +24,11 @@ optional arguments:
   -s MIN_SILENCE_LEN    min_silence_len (ms), default = 300
   -t SILENCE_THRESH     silence threshold, default = -16
   -m MARGIN             margin (ms), default = 100
+  -n N_SEGMENTS         number of chunks to split input file to be processed independently
+                        default = 10
   -d                    print debugging info
 ```
 
 ## Example:
 
-```python3 main.py -i lect1.mp4 -o lect1_trimmed.mp4 -d```
+```python3 main.py -i lect1.mp4 -o lect1_trimmed.mp4 -d ```
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import argparse
 import logging
 import os
 import pathlib
 import shutil
+import subprocess
 import tempfile
-import argparse
+from typing import List
+
 import ffmpeg
 from pydub import AudioSegment, silence
 
@@ -17,13 +20,48 @@ def time_format(ms):
     sec %= 60
     hours = minutes // 60
     minutes %= 60
-    return "%02d:%02d:%02d.%d" % (hours, minutes, sec, msec)
+    return "%02d:%02d:%02d.%03d" % (hours, minutes, sec, msec)
 
 
 basename = "lecture"
+chunks_count = 40
+ffmpeg_cmd = ["ffmpeg", "-hide_banner", "-loglevel", "error"]
 
 
-def trim_silence(infile, outfile, min_silence_len, silence_thresh, margin):
+def get_length(filename: str) -> float:
+    result = subprocess.run(["ffprobe", "-v", "error", "-show_entries",
+                             "format=duration", "-of",
+                             "default=noprint_wrappers=1:nokey=1", filename],
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT)
+    return float(result.stdout)
+
+
+def concatenate_videos(chunks: List[str], outfile: str):
+    list_file = tempfile.NamedTemporaryFile("w", suffix='.txt')
+    for file in chunks:
+        print(f"file '{file}'", file=list_file)
+    list_file.flush()
+    logging.info(f'{len(chunks)} chunks written to {list_file.name}')
+
+    logging.info(f'Concatenating chunks into {outfile}')
+
+    concat_args = ffmpeg_cmd + [
+        "-safe", "0",
+        "-f", "concat",
+        "-i", str(list_file.name),
+        "-c", "copy",
+        outfile
+    ]
+    subprocess.check_output(concat_args)
+
+
+def trim_silence(infile: str, outfile: str, min_silence_len: int, silence_thresh: int, margin: int) -> bool:
+    """
+    Returns
+    -------
+    True trimming was successful and resulted in nonempty outfile
+    """
     workdir = tempfile.mkdtemp()
     shutil.copy(os.path.join(workdir, infile), os.path.join(workdir, basename + pathlib.Path(infile).suffix))
 
@@ -47,13 +85,16 @@ def trim_silence(infile, outfile, min_silence_len, silence_thresh, margin):
         f"Detected {len(parts)} nonsilent parts, total duration: {sum((stop - start) for start, stop in parts)} ms")
     logging.info(parts)
 
+    if len(parts) == 0:
+        return False
+
     logging.info("Trimming audio")
     trimmed_audio = AudioSegment.empty()
     for start, stop in parts:
         trimmed_audio += audio[start:stop]
     logging.info("Done trimming audio")
 
-    logging.info(f"Writing trimmed audio into {infile_audio} with duration {trimmed_audio.duration_seconds}")
+    logging.info(f"Writing trimmed audio into {infile_audio} with duration {trimmed_audio.duration_seconds} s.")
     trimmed_audio.export(infile_audio)
 
     parts = [(start / 1000, stop / 1000) for (start, stop) in parts]
@@ -68,6 +109,27 @@ def trim_silence(infile, outfile, min_silence_len, silence_thresh, margin):
         v=1,
         a=1).node
     ffmpeg.output(joined[0], joined[1], outfile).run(quiet=True, overwrite_output=True)
+    return True
+
+
+def split_video(infile: str, output_dir: str, prefix: str, n_parts: int) -> List[str]:
+    duration = round(get_length(infile) * 1000)
+    split_args = ffmpeg_cmd + ["-i", infile]
+    segments = []
+    for i in range(n_parts):
+        filename = os.path.join(output_dir, f"{prefix}_{i}{pathlib.Path(infile).suffix}")
+        split_args += [
+            "-ss", time_format(round(duration * i) / n_parts),
+            "-to", time_format(round(duration * (i + 1) / n_parts)),
+            "-c", "copy",
+            filename
+        ]
+        segments.append(filename)
+    logging.info(f"Splitting {infile} into {n_parts} segments")
+    subprocess.check_output(split_args)
+    logging.info(f"Done splitting {infile}")
+    logging.info(segments)
+    return segments
 
 
 if __name__ == '__main__':
@@ -80,11 +142,25 @@ if __name__ == '__main__':
                         help='silence threshold', default=-16)
     parser.add_argument('-m', dest='margin', type=int,
                         help='margin (ms)', default=100)
-
+    parser.add_argument('-n', dest='n_segments', type=int,
+                        help='number of chunks to split input file to be processed independently', default=10)
     parser.add_argument('-d', dest='log_level', action='store_const', const=logging.DEBUG, help='print debugging info',
                         default=logging.WARNING)
     args = parser.parse_args()
 
     logging.basicConfig(format="%(asctime)s - %(message)s", level=args.log_level)
 
-    trim_silence(args.infile, args.outfile, args.min_silence_len, args.silence_thresh, args.margin)
+    workdir = tempfile.mkdtemp()
+    segments = split_video(args.infile, workdir, basename, args.n_segments)
+    cropped_segments = []
+    for id, segment in enumerate(segments):
+        filename = os.path.join(workdir, basename + f"_cropped_{id}_" + pathlib.Path(args.infile).suffix)
+        logging.info(f"Processing chunk {id + 1}/{len(segments)}: {segment} -> {filename}")
+        if trim_silence(segment,
+                        filename,
+                        args.min_silence_len,
+                        args.silence_thresh,
+                        args.margin):
+            cropped_segments.append(filename)
+        logging.info(f"Done processing chunk {id + 1}/{len(segments)}")
+    concatenate_videos(cropped_segments, args.outfile)
